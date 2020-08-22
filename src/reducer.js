011076f46c777dd7dbe4d64d2e86c5fc1474058f
LastHouse <54175608+LastHouse@users.noreply.github.com>
2020-08-22 15:56:14 +0300

    Parts 6.1 & 6.2 done

--- src/reducer.js
+++ src/reducer.js
@@ -1,23 +1,35 @@
 const initialState = {
   good: 0,
   ok: 0,
-  bad: 0
-}
+  bad: 0,
+};
 
 const counterReducer = (state = initialState, action) => {
-  console.log(action)
+  console.log(action);
   switch (action.type) {
     case 'GOOD':
-      return state
+      const changedGood = {
+        ...state,
+        good: state.good + 1,
+      };
+      return changedGood;
     case 'OK':
-      return state
+      const changedOk = {
+        ...state,
+        ok: state.ok + 1,
+      };
+      return changedOk;
     case 'BAD':
-      return state
+      const changedBad = {
+        ...state,
+        bad: state.bad + 1,
+      };
+      return changedBad;
     case 'ZERO':
-      return state
-    default: return state
+      return initialState;
+    default:
+      return state;
   }
-  
-}
+};
 
-export default counterReducer
+export default counterReducer;
